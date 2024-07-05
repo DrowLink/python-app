e52e7eb006fd44889833f60bd2d4182b1d10bde5
DrowLink <pauloriveiro01@gmail.com>
2024-07-05 15:50:07 -0400

    first commit

---
+++ ecommerce/back/main.py
@@ -0,0 +1,28 @@
+
+
+#Comprueba si un numero es impar o par
+
+
+num = input("Ingrese su numero: ")
+
+
+class algoritmoNumeros:
+    def __init__(self):
+        self.num = num
+
+
+    def esParElNumero():
+        if (num % 2 == 0):
+            print("El numero es par")
+        else:
+            print("El numero es impar")
+
+
+
+def juntarListas():
+    data = ["1", "2", "3", "4", "5"]
+    arr += data
+
+
+
+print() 
